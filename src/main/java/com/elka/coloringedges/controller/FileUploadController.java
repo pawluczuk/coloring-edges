7b3722507fb67db5d838bcde970260cd0ac1d51a
lemcz <bartek251@op.pl>
2016-06-09 08:40:34 +0200

    feat: added output file with graph statistics

--- src/main/java/com/elka/coloringedges/controller/FileUploadController.java
+++ src/main/java/com/elka/coloringedges/controller/FileUploadController.java
@@ -1,5 +1,6 @@
 package com.elka.coloringedges.controller;
 
+import com.elka.coloringedges.domain.Edge;
 import com.elka.coloringedges.domain.Graph;
 import com.elka.coloringedges.service.GraphService;
 import com.elka.coloringedges.service.ImportService;
@@ -12,7 +13,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @Controller
 public class FileUploadController {
@@ -42,6 +50,17 @@ public class FileUploadController {
                 log.info("Max graph colors calculated: " + graph.getEdgeWithMaxColor().getColor());
 
                 redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + originalFileName + "!");
+                List<String> fileLines = new ArrayList<>();
+                fileLines.add("Vertices: " + graph.getVertices().size());
+                fileLines.add("Edges: " + graph.getEdges().size());
+                fileLines.add("Delta(G): " + graph.getDeltaGraph());
+                fileLines.add("Used colors: " + graph.getEdgeWithMaxColor().getColor());
+
+                fileLines.addAll(graph.getEdges().stream()
+                        .map(edge -> edge.getSourceVertex().getId() + " " + edge.getDestinationVertex().getId() + " " + edge.getColor())
+                        .collect(Collectors.toList()));
+                Path outputFile = Paths.get("output.txt");
+                Files.write(outputFile, fileLines, Charset.forName("UTF-8"));
                 return graph;
             }
             catch (Exception e) {
