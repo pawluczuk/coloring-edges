08e068f016e7e382d643951bf324368835634374
lemc <blemiec@gmail.com>
2016-06-11 16:08:42 +0200

    fix: fixed coloring (added recoloring for bepartite graphs)

--- src/main/java/com/elka/coloringedges/service/GraphService.java
+++ src/main/java/com/elka/coloringedges/service/GraphService.java
@@ -8,6 +8,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Logger;
 
 @Service
@@ -55,20 +56,73 @@ public class GraphService {
     }
 
     public Graph colorEdges(Graph graph) {
-        for (Vertex vertex : graph.getVertices()) {
-            for (Edge edge : vertex.getEdges()) {
-                if (edge.getColor() == -1) {
-                    Integer i = 1;
-                    Set<Integer> colorsSet = edge.getSourceVertex().getUsedColors();
-                    colorsSet.addAll(edge.getDestinationVertex().getUsedColors());
-                    while (colorsSet.contains(i)) {
-                        i++;
-                    }
+        int deltaG = graph.getDeltaGraph();
+        boolean bipartite = graph.isBipartite();
+        int k = bipartite ? deltaG : deltaG + 1;
+        for (Edge edge : graph.getEdges()) {
+            if (edge.getColor() == -1) {
+                Integer i = 1;
+                Set<Integer> colorsSet = edge.getSourceVertex().getUsedColors();
+                colorsSet.addAll(edge.getDestinationVertex().getUsedColors());
+                while (colorsSet.contains(i) && i <= k) {
+                    i++;
+                }
+                if (i > k && bipartite) {
+                    edge.setColor(recolor(edge.getSourceVertex(), edge.getDestinationVertex()));
+                } else {
                     edge.setColor(i);
-                    colorsSet.add(i);
                 }
             }
         }
         return graph;
+    }
+
+    private Integer recolor(Vertex source, Vertex target) {
+
+        Map<Object, Integer> switchColors = new HashMap<>();
+
+        for (Integer i : source.getUsedColors()) {
+            if(!target.getUsedColors().contains(i)) {
+                switchColors.put("source", i);
+                break;
+            }
+        }
+        for (Integer i : target.getUsedColors()) {
+            if(!source.getUsedColors().contains(i)) {
+                switchColors.put("target", i);
+                break;
+            }
+        }
+
+        switchColors.put(switchColors.get("source"), switchColors.get("target"));
+        switchColors.put(switchColors.get("target"), switchColors.get("source"));
+
+        Set<Edge> edges = new HashSet<>();
+
+        String[] foo = new String[2];
+        foo[0] = "source";
+        foo[1] = "target";
+
+        int j = 0;
+        while(true) {
+            foo: {
+                for (Edge edge : source.getEdges()) {
+                    if (Objects.equals(edge.getColor(), switchColors.get(foo[j % 2]))) {
+                        if (!edges.contains(edge)) {
+                            edges.add(edge);
+                            j++;
+                            source = edge.getDestinationVertex().equals(source) ? edge.getSourceVertex() : edge.getDestinationVertex();
+                            break foo;
+                        }
+                    }
+                }
+                break;
+            }
+        }
+
+        for (Edge edge : edges) {
+            edge.setColor(switchColors.get(edge.getColor()));
+        }
+        return switchColors.get("source");
     }
 }
